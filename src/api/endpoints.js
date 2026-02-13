ed2b9f556e3eedba2e86ff465713b5c79fd70a68
Pranavo <158192327+PRANAVO02@users.noreply.github.com>
2026-02-13 19:21:56 +0530

    updated URL for deployment

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://golancebackend.onrender.com/api";
+// const BASE_URL = "http://localhost:8080/api";
 
 export const ENDPOINTS = {
   // 🔹 Authentication
